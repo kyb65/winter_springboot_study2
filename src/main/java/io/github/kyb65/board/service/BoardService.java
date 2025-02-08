aab2f6dd834236c645f495f684cb04d51522af6d
Kim Yebon <yebon.kim0605@gmail.com>
2025-02-08 17:50:39 +0900

    강의 4 2025.02.08 글 목록 조회 기능. 글 제목이 링크로 작동함!!!

--- src/main/java/io/github/kyb65/board/service/BoardService.java
+++ src/main/java/io/github/kyb65/board/service/BoardService.java
@@ -7,6 +7,9 @@ import io.github.kyb65.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // dto -> entity (entity class)
 // entity -> dto (dto class)
 // 이런 변환 과정이 필요함.
@@ -19,5 +22,17 @@ public class BoardService {
     public void save(BoardDTO boardDTO) {
         BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
         boardRepository.save(boardEntity);
+    }
+
+
+    public List<BoardDTO> findAll() {
+
+        // entity 객체를 dto 객체로 옮겨담기.
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        for (BoardEntity boardEntity: boardEntityList) {
+            boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
+        }
+        return boardDTOList;
     }
 }
