a212ede17717df976cac15a5420472dd107896ae
Kim Yebon <yebon.kim0605@gmail.com>
2025-03-22 13:27:25 +0900

    강의 8 2025.03.22 페이징 기능 추가.

--- src/main/java/io/github/kyb65/board/controller/BoardController.java
+++ src/main/java/io/github/kyb65/board/controller/BoardController.java
@@ -3,6 +3,9 @@ package io.github.kyb65.board.controller;
 import io.github.kyb65.board.dto.BoardDTO;
 import io.github.kyb65.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -65,5 +68,27 @@ public class BoardController {
         System.out.println("1");
         boardService.delete(id);
         return "redirect:/board/";
+    }
+
+    // /board/paging?page=1
+    @GetMapping("/paging")
+    public String paging(@PageableDefault(page=1) Pageable pageable, Model model){
+//        pageable.getPageNumber();
+        Page<BoardDTO> boardList = boardService.paging(pageable);
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
+
+        // page 개수 20개
+        // 현재 사용자가 3페이지를 보고 있음.
+        // 1 2 3
+        // 현재 사용자가 7페이지
+        // 7 8 9
+        // 보여지는 페이지 갯수 3개
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        return "paging";
     }
 }
