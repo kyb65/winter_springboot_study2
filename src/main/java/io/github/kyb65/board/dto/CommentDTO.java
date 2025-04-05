bd29cc5d1b83c108386689ced8d3ce007b694ffa
Kim Yebon <yebon.kim0605@gmail.com>
2025-04-05 23:06:11 +0900

    강의 10 2025.04.05 댓글 입력 기능 작성 - 댓글 작성자, 댓글 내용, 게시글 번호 받아오는 작업 완료

---
+++ src/main/java/io/github/kyb65/board/dto/CommentDTO.java
@@ -0,0 +1,19 @@
+package io.github.kyb65.board.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class CommentDTO {
+    private Long id;
+    private String commentWriter;
+    private String commentContents;
+    private Long boardId;
+    private LocalDateTime commentCreatedTime;
+}
